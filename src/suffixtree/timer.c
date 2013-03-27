91e597c42e9954df124376395ada4c6016466f53
Jeff Daily <jeff.daily@pnnl.gov>
2013-03-27 11:46:28 -0700

    delete unused files in newly added suffix tree dir

--- src/suffixtree/timer.c
+++
@@ -1,5 +0,0 @@
-#include <sys/mta_task.h>
-
-double timer(){
-  return ((double)mta_get_clock(0) / (double)mta_clock_freq());
-}
--- src/suffixtree/timer.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIME_H_
-#define TIME_H_
-
-
-double timer();
-
-#endif /* end of time.h */
