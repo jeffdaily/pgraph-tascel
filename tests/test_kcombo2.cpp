60caa20056af3556d04d2605f06d0d5ed798b55d
Jeff Daily <jeff.daily@pnnl.gov>
2013-11-16 13:00:46 -0800

    fixes for OSX

--- tests/test_kcombo2.cpp
+++ tests/test_kcombo2.cpp
@@ -1,6 +1,12 @@
 #include "config.h"
 
+#include <time.h>
 #include <sys/time.h>
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 
 #include <cassert>
 #include <cstdio>
@@ -19,9 +25,19 @@ static double wtime()
     (void)gettimeofday(&tv, NULL);
     return double(tv.tv_sec) + double(tv.tv_usec) / 1000000.0;
 #else
-    timespec ts;
+    struct timespec ts;
+#   if defined(__MACH__) // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
+#   else
     // clock_gettime(CLOCK_MONOTONIC, &ts); // Works on FreeBSD
     clock_gettime(CLOCK_REALTIME, &ts); // Works on Linux
+#   endif
     return double(ts.tv_sec) + double(ts.tv_nsec) / 1000000000.0;
 #endif
 }
