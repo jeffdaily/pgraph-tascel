7d6ddc0e7f08813fc1b8ceb621554871ed409d04
Jeff Daily <jeff.daily@pnnl.gov>
2013-12-09 12:21:08 -0800

    forgot to add SequenceDatabaseReplicated files

---
+++ src/SequenceDatabaseReplicated.cpp
@@ -0,0 +1,523 @@
+/**
+ * @file SequenceDatabaseReplicated.cpp
+ *
+ * @author jeff.daily@pnnl.gov
+ *
+ * @todo TODO handle fasta files with multiline sequences
+ *
+ * Copyright 2012 Pacific Northwest National Laboratory. All rights reserved.
+ */
+#include "config.h"
+
+#include <mpi.h>
+
+#include <cassert>
+#include <cctype>
+#include <cstddef>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <utility>
+
+#include <tascel.h>
+
+#include "alignment.hpp"
+#include "csequence.h"
+#include "Sequence.hpp"
+#include "SequenceDatabase.hpp"
+#include "SequenceDatabaseReplicated.hpp"
+#include "mpix.hpp"
+
+using std::accumulate;
+using std::cerr;
+using std::endl;
+using std::ifstream;
+using std::make_pair;
+using std::ostringstream;
+using std::size_t;
+using std::string;
+using tascel::LockGuard;
+using tascel::PthreadMutex;
+
+#define TAG 2345
+
+namespace pgraph {
+
+
+SequenceDatabaseReplicated::SequenceDatabaseReplicated(
+        const string &file_name,
+        size_t budget,
+        MPI_Comm comm,
+        size_t num_threads,
+        char delimiter)
+    :   SequenceDatabase()
+    ,   comm_orig(comm)
+    ,   comm_orig_rank(0)
+    ,   comm_orig_size(0)
+    ,   comm(MPI_COMM_NULL)
+    ,   comm_rank(0)
+    ,   comm_size(0)
+    ,   budget(budget)
+    ,   num_threads(num_threads)
+    ,   delimiter(delimiter)
+    ,   file_name(file_name)
+    ,   local_data(NULL)
+    ,   local_cache()
+    ,   local_size(0)
+    ,   max_seq_size(0)
+    ,   tbl(NULL)
+    ,   del(NULL)
+    ,   ins(NULL)
+{
+    int ierr = 0;
+
+    /* rank and size */
+    ierr = MPI_Comm_rank(comm_orig, &comm_orig_rank);
+    MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+    ierr = MPI_Comm_size(comm_orig, &comm_orig_size);
+    MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+
+    read_and_parse_fasta();
+}
+
+
+SequenceDatabaseReplicated::~SequenceDatabaseReplicated()
+{
+    map<size_t, Sequence*>::iterator it;
+
+    for (it=local_cache.begin(); it!=local_cache.end(); ++it) {
+        delete it->second;
+    }
+
+    for (size_t worker=0; worker<num_threads; ++worker) {
+#if 1
+        delete [] tbl[worker][1];
+        delete [] tbl[worker][0];
+        delete [] tbl[worker];
+        delete [] del[worker][1];
+        delete [] del[worker][0];
+        delete [] del[worker];
+        delete [] ins[worker][1];
+        delete [] ins[worker][0];
+        delete [] ins[worker];
+#else
+        free_cell_table(2, tbl[worker]);
+        free_int_table(2, del[worker]);
+        free_int_table(2, ins[worker]);
+#endif
+    }
+    delete [] tbl;
+    delete [] del;
+    delete [] ins;
+    delete [] local_data;
+}
+
+
+size_t SequenceDatabaseReplicated::get_local_count() const
+{
+    return local_cache.size();
+}
+
+
+size_t SequenceDatabaseReplicated::get_local_size() const
+{
+    return local_size;
+}
+
+
+size_t SequenceDatabaseReplicated::get_global_count() const
+{
+    return get_local_count();
+}
+
+
+size_t SequenceDatabaseReplicated::get_global_size() const
+{
+    return get_local_size();
+}
+
+
+void SequenceDatabaseReplicated::read_and_parse_fasta()
+{
+    MPI_File in = MPI_FILE_NULL;
+    MPI_Offset file_size = 0;
+    MPI_Offset budget = MPI_Offset(this->budget);
+    int ierr = 0;
+
+    /* open file and get file size on rank 0*/
+    if (0 == comm_orig_rank) {
+        ierr = MPI_File_open(MPI_COMM_SELF,
+                const_cast<char *>(file_name.c_str()),
+                MPI_MODE_RDONLY, MPI_INFO_NULL, &in);
+        MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+        ierr = MPI_File_get_size(in, &file_size);
+        MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+        ierr = MPI_File_close(&in);
+        MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+    }
+    mpix_bcast(file_size, 0, comm_orig);
+
+    mpix_print_zero("memory budget", budget, comm_orig);
+    mpix_print_zero("file size", file_size, comm_orig);
+    
+    if (budget >= file_size) {
+        comm = comm_orig;
+        comm_rank = comm_orig_rank;
+        comm_size = comm_orig_size;
+        ierr = MPI_File_open(MPI_COMM_SELF,
+                const_cast<char *>(file_name.c_str()),
+                MPI_MODE_RDONLY, MPI_INFO_NULL, &in);
+        MPI_CHECK_IERR(ierr, comm_orig_rank, comm_orig);
+        read_and_parse_fasta_himem(in, file_size);
+    }
+    else {
+        assert(0);
+    }
+}
+
+
+void SequenceDatabaseReplicated::read_and_parse_fasta_himem(MPI_File in,
+                                                  MPI_Offset file_size)
+{
+    int ierr = 0;
+    size_t new_size = 0;
+
+    /* read directly into a local buffer */
+    local_data = new char[file_size+2]; /* +2 for last delim and null */
+    local_data[file_size] = delimiter;
+    local_data[file_size+1] = '\0';
+
+    /* everyone reads in their part */
+    ierr = MPI_File_read_at_all(in, 0, local_data, file_size,
+            MPI_CHAR, MPI_STATUS_IGNORE);
+    MPI_CHECK_IERR(ierr, comm_rank, comm);
+
+    /* everyone can close the file now */
+    ierr = MPI_File_close(&in);
+    MPI_CHECK_IERR(ierr, comm_rank, comm);
+
+    /* pack and index the fasta buffer */
+    pack_and_index_fasta(local_data, file_size, delimiter, 0, new_size);
+    local_data[new_size] = delimiter;
+    local_data[new_size+1] = '\0';
+    assert(!local_cache.empty());
+    assert(local_cache.size() > 0);
+}
+
+
+void SequenceDatabaseReplicated::pack_and_index_fasta(char *buffer,
+                                            size_t size,
+                                            char delimiter,
+                                            size_t id,
+                                            size_t &new_size)
+{
+    size_t r = 0;           /* read index */
+    size_t w = 0;           /* write index */
+    size_t last_gt = 0;     /* index of last '>' seen */
+    size_t last_hash = 0;   /* index of last '#' inserted */
+
+    new_size = 0;
+
+    /* We skip all newlines unless it is the newline that terminates the
+     * sequence ID. We replace the ID-terminating newline with '#'. We replace
+     * the sequence terminating newline with the given delimiter. */
+
+    if (buffer[0] != '>') {
+        cerr << '[' << comm_rank << "] buffer[0] != '>'" << endl;
+        MPI_Abort(comm, -1);
+    }
+
+    while (r<size) {
+        if (buffer[r] == '>') {
+            last_gt = w;
+            while (r<size && buffer[r] != '\n') {
+                buffer[w++] = buffer[r++];
+            }
+            last_hash = w;
+            buffer[w++] = '#';
+            r++;
+        }
+        else {
+            while (r<size && buffer[r] != '\n') {
+                buffer[w++] = buffer[r++];
+            }
+            /* peek at next character, either EOF or '>' */
+            if (r<size) {
+                if (r+1 >= size || buffer[r+1] == '>') {
+                    buffer[w++] = delimiter;
+                    size_t id_offset = 0;
+                    size_t id_length = last_hash - last_gt;
+                    size_t sequence_offset = last_hash - last_gt + 1;
+                    size_t sequence_length = w - last_hash - 1;
+                    if (delimiter == '\0') {
+                        sequence_length -= 1;
+                    }
+                    local_cache[id++] =
+                        new Sequence(&buffer[last_gt],
+                                     id_offset,
+                                     id_length,
+                                     sequence_offset,
+                                     sequence_length);
+                    assert(!local_cache.empty());
+                    assert(local_cache.size() > 0);
+                    {
+                        size_t l = local_cache[id-1]->get_sequence_length();
+                        max_seq_size = l > max_seq_size ? l : max_seq_size;
+                        local_size += l;
+                    }
+                }
+            }
+            /* or file wasn't terminated with a newline */
+            else {
+                /* hopefully space enough to append the delimiter anyway */
+                assert(w<r);
+                buffer[w++] = delimiter;
+                local_cache[id++] =
+                    new Sequence(&buffer[last_gt],
+                                 0,
+                                 last_hash - last_gt,
+                                 last_hash - last_gt + 1,
+                                 w - last_hash - 1);
+                assert(!local_cache.empty());
+                assert(local_cache.size() > 0);
+                {
+                    size_t l = local_cache[id-1]->get_sequence_length();
+                    max_seq_size = l > max_seq_size ? l : max_seq_size;
+                    local_size += l;
+                }
+                assert(0);
+            }
+            r++;
+        }
+    }
+
+    assert(!local_cache.empty());
+    assert(local_cache.size() > 0);
+
+    new_size = w;
+
+    mpix_allreduce(max_seq_size, MPI_MAX, comm);
+    mpix_print_zero("max_seq_size", max_seq_size, comm);
+
+    assert(num_threads > 0);
+    tbl = new cell_t**[num_threads];
+    del = new int**[num_threads];
+    ins = new int**[num_threads];
+    for (size_t worker=0; worker<num_threads; ++worker) {
+#if 1
+        size_t max_seq_size_p1 = max_seq_size + 1;
+        tbl[worker] = new cell_t*[2];
+        tbl[worker][0] = new cell_t[max_seq_size_p1];
+        tbl[worker][1] = new cell_t[max_seq_size_p1];
+        del[worker] = new int*[2];
+        del[worker][0] = new int[max_seq_size_p1];
+        del[worker][1] = new int[max_seq_size_p1];
+        ins[worker] = new int*[2];
+        ins[worker][0] = new int[max_seq_size_p1];
+        ins[worker][1] = new int[max_seq_size_p1];
+#if USE_MEMSET
+        (void)memset(tbl[worker][0], 0, sizeof(cell_t)*max_seq_size_p1);
+        (void)memset(tbl[worker][1], 0, sizeof(cell_t)*max_seq_size_p1);
+        (void)memset(del[worker][0], 0, sizeof(int)*max_seq_size_p1);
+        (void)memset(del[worker][1], 0, sizeof(int)*max_seq_size_p1);
+        (void)memset(ins[worker][0], 0, sizeof(int)*max_seq_size_p1);
+        (void)memset(ins[worker][1], 0, sizeof(int)*max_seq_size_p1);
+#endif
+#else
+        tbl[worker] = allocate_cell_table(2, max_seq_size);
+        del[worker] = allocate_int_table(2, max_seq_size);
+        ins[worker] = allocate_int_table(2, max_seq_size);
+#endif
+    }
+}
+
+
+
+Sequence &SequenceDatabaseReplicated::get_sequence(size_t i)
+{
+    return *local_cache[i];
+}
+
+
+Sequence &SequenceDatabaseReplicated::operator[](size_t i)
+{
+    return this->get_sequence(i);
+}
+
+
+void SequenceDatabaseReplicated::set_num_threads(size_t num)
+{
+    assert(0); // not used yet
+    assert(tbl == NULL);
+    assert(del == NULL);
+    assert(ins == NULL);
+
+    tbl = new cell_t**[num];
+    assert(tbl);
+    del = new int**[num];
+    assert(del);
+    ins = new int**[num];
+    assert(ins);
+
+    for (size_t tid=0; tid<num; ++tid) {
+        tbl[tid] = NULL;
+        del[tid] = NULL;
+        ins[tid] = NULL;
+    }
+}
+
+
+void SequenceDatabaseReplicated::align(size_t i,
+                                  size_t j,
+                                  int &score,
+                                  int &ndig,
+                                  int &alen,
+                                  int open,
+                                  int gap,
+                                  int tid)
+{
+    Sequence &s1 = get_sequence(i);
+    Sequence &s2 = get_sequence(j);
+    const char *c1 = NULL;
+    const char *c2 = NULL;
+    size_t l1 = 0;
+    size_t l2 = 0;
+    cell_t result;
+
+    assert(max_seq_size > 0);
+
+#if 0
+    if (tbl[tid] == NULL) {
+        tbl[tid] = new cell_t*[2];
+        tbl[tid][0] = new cell_t[max_seq_size];
+        tbl[tid][1] = new cell_t[max_seq_size];
+        assert(del[tid] == NULL);
+        del[tid] = new int*[2];
+        del[tid][0] = new int[max_seq_size];
+        del[tid][1] = new int[max_seq_size];
+        assert(ins[tid] == NULL);
+        ins[tid] = new int*[2];
+        ins[tid][0] = new int[max_seq_size];
+        ins[tid][1] = new int[max_seq_size];
+    }
+#else
+    assert(tbl);
+    assert(tbl[tid]);
+    assert(tbl[tid][0]);
+    assert(tbl[tid][1]);
+    assert(del);
+    assert(del[tid]);
+    assert(del[tid][0]);
+    assert(del[tid][1]);
+    assert(ins);
+    assert(ins[tid]);
+    assert(ins[tid][0]);
+    assert(ins[tid][1]);
+#endif
+
+    s1.get_sequence(c1,l1);
+    assert(c1);
+    assert(l1);
+    s2.get_sequence(c2,l2);
+    assert(c2);
+    assert(l2);
+    result = affine_gap_align_blosum(c1, l1, c2, l2,
+            tbl[tid], del[tid], ins[tid], open, gap);
+
+    score = result.score;
+    ndig = result.matches;
+    alen = result.length;
+}
+
+
+void SequenceDatabaseReplicated::align_ssw(size_t i,
+                                      size_t j,
+                                      int &score,
+                                      int &ndig,
+                                      int &alen,
+                                      int open,
+                                      int gap,
+                                      int tid)
+{
+    Sequence &s1 = get_sequence(i);
+    Sequence &s2 = get_sequence(j);
+    const char *c1 = NULL;
+    const char *c2 = NULL;
+    size_t l1 = 0;
+    size_t l2 = 0;
+    cell_t result;
+
+    assert(max_seq_size > 0);
+
+    s1.get_sequence(c1,l1);
+    assert(c1);
+    assert(l1);
+    s2.get_sequence(c2,l2);
+    assert(c2);
+    assert(l2);
+    result = affine_gap_align_blosum_ssw(c1, l1, c2, l2, open, gap);
+
+    score = result.score;
+    ndig = result.matches;
+    alen = result.length;
+}
+
+
+bool SequenceDatabaseReplicated::is_edge(size_t i,
+                                    size_t j,
+                                    const int &score,
+                                    const int &ndig,
+                                    const int &alen,
+                                    const int &AOL,
+                                    const int &SIM,
+                                    const int &OS,
+                                    int &sscore,
+                                    size_t &max_len)
+{
+    Sequence &s1 = get_sequence(i);
+    Sequence &s2 = get_sequence(j);
+    const char *c1;
+    const char *c2;
+    size_t l1;
+    size_t l2;
+    int nmatch = 0;
+
+    /* DO NOT need to compute the sscore value every time, if the later check
+     * failed at the first step, the sscore computation is wasted */
+    s1.get_sequence(c1,l1);
+    s2.get_sequence(c2,l2);
+
+    assert(l1);
+    assert(l2);
+
+    if (l1 > l2) {
+        max_len = l1;
+        sscore = self_score_blosum(c1,l1);
+    }
+    else {
+        max_len = l2;
+        sscore = self_score_blosum(c2,l2);
+    }
+
+    nmatch = ndig;
+
+    /* order the condition in strict->loose way, performance perspective
+     * comparison using integers, no overflow could happen */
+    if (score <= 0) {
+        return false;
+    }
+    else if ((alen * 100 >= AOL * int(max_len))
+             && (nmatch * 100 >= SIM * alen)
+             && (score * 100 >= OS * sscore)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+}; /* namespace pgraph */
+
---
+++ src/SequenceDatabaseReplicated.hpp
@@ -0,0 +1,246 @@
+/**
+ * @file SequenceDatabaseReplicated.hpp
+ *
+ * @author jeff.daily@pnnl.gov
+ *
+ * Copyright 2012 Pacific Northwest National Laboratory. All rights reserved.
+ */
+#ifndef SEQUENCE_DATABASE_REPLICATED_H_
+#define SEQUENCE_DATABASE_REPLICATED_H_
+
+#include <cstddef>
+#include <exception>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <mpi.h>
+
+#include "Sequence.hpp"
+#include "SequenceDatabase.hpp"
+
+using std::exception;
+using std::map;
+using std::size_t;
+using std::string;
+using std::vector;
+
+namespace pgraph {
+
+
+/**
+ * Collection of ordered Sequence instances, indexed from 0 to N.
+ */
+class SequenceDatabaseReplicated : public SequenceDatabase
+{
+    public:
+
+        /**
+         * Creates a sequence database from the given file with the given
+         * memory budget.
+         *
+         * @pre !filename.empty()
+         * @param[in] filename the file to open (fasta format)
+         * @param[in] budget the memory budget, 0 for unlimited
+         * @param[in] comm MPI communicator
+         * @param[in] num_threads number of threads which migth access this DB
+         * @param[in] delimiter used to separate sequences
+         */
+        SequenceDatabaseReplicated(const string &filename,
+                              size_t budget,
+                              MPI_Comm comm,
+                              size_t num_threads=1,
+                              char delimiter='$');
+
+        /**
+         * Destroys the SequenceDatabaseReplicated.
+         */
+        virtual ~SequenceDatabaseReplicated();
+
+        /**
+         * Returns how many sequences are stored on this process.
+         *
+         * @return how many sequences are stored on this process.
+         */
+        virtual size_t get_local_count() const;
+
+        /**
+         * Returns how many characters are stored on this process.
+         *
+         * @return how many characters are stored on this process.
+         */
+        virtual size_t get_local_size() const;
+
+        /**
+         * Returns how many sequences are stored collectively by every process.
+         *
+         * @return how many sequences are stored collectively by every process.
+         */
+        virtual size_t get_global_count() const;
+
+        /**
+         * Returns how many complete sets of sequences are stored collectively by every process.
+         *
+         * @return how many complete sets of sequences are stored collectively by every process.
+         */
+        virtual size_t get_global_replica_count() const { return comm_size; }
+
+        /**
+         * Returns the index of this replica, 0-based.
+         *
+         * @return the index of this replica, 0-based.
+         */
+        virtual size_t get_global_replica_index() const { return comm_rank; }
+
+        /**
+         * Returns how many characters are stored collectively by every process.
+         *
+         * @return how many characters are stored collectively by every process.
+         */
+        virtual size_t get_global_size() const;
+
+        /**
+         * Returns a reference to the Sequence based on the global index i.
+         *
+         * @param[in] i the index based on the global count of sequences
+         * @return the Sequence reference (owned by this SequenceDatabaseReplicated)
+         */
+        virtual Sequence &get_sequence(size_t i);
+
+        /**
+         * Returns a reference to the Sequence based on the global index i.
+         *
+         * @param[in] i the index based on the global count of sequences
+         * @return the Sequence reference (owned by this SequenceDatabaseReplicated)
+         */
+        virtual Sequence &operator[](size_t i);
+
+        /**
+         * Sets the number of threads that might possibly access this db.
+         *
+         * @param[in] num the number of threads
+         */
+        virtual void set_num_threads(size_t num);
+
+        /**
+         * Gets the size of the largest sequence in this database.
+         *
+         * @return the size of the largest sequence in this database.
+         */
+        virtual size_t get_max_length() const { return max_seq_size; }
+
+        /**
+         * Computes the alignment between the two sequence IDs.
+         *
+         * @param[in] i first Sequence index
+         * @param[in] j second Sequence index
+         * @param[out] score alignment score
+         * @param[out] ndig number of matches
+         * @param[out] align alignment length
+         * @param[in] open penalty
+         * @param[in] gap penalty
+         * @param[in] tid thread ID, defaulting to 0
+         */
+        void align(size_t i,
+                   size_t j,
+                   int &score,
+                   int &ndig,
+                   int &align,
+                   int open=-10,
+                   int gap=-1,
+                   int tid=0);
+
+        /**
+         * Computes the alignment between the two sequence IDs.
+         *
+         * @param[in] i first Sequence index
+         * @param[in] j second Sequence index
+         * @param[out] score alignment score
+         * @param[out] ndig number of matches
+         * @param[out] align alignment length
+         * @param[in] open penalty
+         * @param[in] gap penalty
+         * @param[in] tid thread ID, defaulting to 0
+         */
+        void align_ssw(size_t i,
+                   size_t j,
+                   int &score,
+                   int &ndig,
+                   int &align,
+                   int open=-10,
+                   int gap=-1,
+                   int tid=0);
+
+        /**
+         * Computes whether an edge exists between the two sequence IDs.
+         *
+         * @param[in] i first Sequence index
+         * @param[in] j second Sequence index
+         * @param[in] score TODO
+         * @param[in] ndig TODO
+         * @param[in] align TODO
+         * @param[in] AOL TODO
+         * @param[in] SIM TODO
+         * @param[in] OS TODO
+         * @param[out] sscore self score
+         * @param[out] max_len longest of the two sequences 
+         * @return the answer
+         */
+        virtual bool is_edge(size_t i,
+                             size_t j,
+                             const int &score,
+                             const int &ndig,
+                             const int &align,
+                             const int &AOL,
+                             const int &SIM,
+                             const int &OS,
+                             int &sscore,
+                             size_t &max_len);
+
+    private:
+        void read_and_parse_fasta();
+        void read_and_parse_fasta_himem(MPI_File in, MPI_Offset file_size);
+
+        /**
+         * Preprocesses a fasta file buffer.
+         *
+         * We remove all newlines unless it is the newline that terminates the
+         * sequence ID. We replace the ID-terminating newline with '#'. We replace
+         * the sequence terminating newline with the given delimiter.
+         *
+         * @param[in] buffer the fasta file buffer
+         * @param[in] size of the fasta file buffer
+         * @param[in] delimiter to append to each sequence (can be '\0')
+         * @param[in] index of first sequence
+         * @param[out] new_size of the packed buffer
+         */
+        void pack_and_index_fasta(char *buffer, size_t size, char delimiter,
+                                  size_t index, size_t &new_size);
+
+        /**
+         * Exchanges metadata associated with each local cache.
+         */
+        void exchange_local_cache();
+
+        MPI_Comm comm_orig; /**< original communicator */
+        int comm_orig_rank; /**< original communicator rank */
+        int comm_orig_size; /**< original communicator size */
+        MPI_Comm comm;      /**< sub communicator for smaller domain */
+        int comm_rank;      /**< communicator rank */
+        int comm_size;      /**< communicator size */
+        size_t budget;      /**< max amount of memory to use */
+        size_t num_threads; /**< max number of threads using this DB */
+        char delimiter;     /**< delimiter to separate sequences */
+        string file_name;   /**< fasta file name */
+        char *local_data;   /**< memory allocated for local sequences */
+        map<size_t, Sequence*> local_cache; /**< TODO */
+        size_t local_size;/**< local number of characters */
+        size_t max_seq_size;/**< biggest sequence */
+        cell_t ***tbl;      /**< cell table for alignment */
+        int ***del;         /**< del table for alignment */
+        int ***ins;         /**< ins table for alignment */
+};
+
+}; /* namespace pgraph */
+
+#endif /* SEQUENCE_DATABASE_REPLICATED_H_ */
