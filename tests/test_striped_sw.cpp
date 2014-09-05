9f1759623f8f68ec76c7db7ec9780606dde0c5f6
Jeff Daily <jeff.daily@pnnl.gov>
2014-09-05 13:54:57 -0700

    work in progress for sw striped stats

--- tests/test_striped_sw.cpp
+++ tests/test_striped_sw.cpp
@@ -16,7 +16,7 @@
 
 using namespace ::std;
 
-#define DEBUG 0
+#define DEBUG 1
 #define DEBUG_MATCHES 0
 #define DEBUG_LENGTH 0
 #define SHORT_TEST 0
@@ -245,7 +245,7 @@ static const int MAP_BLOSUM_[256] = {
 };
 
 
-int sw(
+static int sw(
         const char * const restrict s1, const int s1Len,
         const char * const restrict s2, const int s2Len,
         const int open, const int gap,
@@ -413,7 +413,233 @@ end:
 }
 
 
-int sg(
+static DP_t sw_stats(
+        const char * const restrict s1, const int s1Len,
+        const char * const restrict s2, const int s2Len,
+        const int open, const int gap,
+        const int8_t * const restrict matrix)
+{
+    /* compute 'query' profile; s1 is the query */
+    const int32_t n = 24; /* number of amino acids in table */
+    int32_t segLen = (s1Len + 7) / 8;
+    __m128i* vProfile = (__m128i*)malloc(n * segLen * sizeof(__m128i));
+    int16_t* t = (int16_t*)vProfile;
+
+    /* Generate query profile rearrange query sequence & calculate the weight
+     * of match/mismatch */
+    for (int32_t nt=0; nt<n; ++nt) {
+        for (int32_t i=0; i<segLen; ++i) {
+            int32_t j = i;
+            for (int32_t segNum=0; segNum<8; ++segNum) {
+                *t++ = j>=s1Len ? 0 : matrix[nt*n + MAP_BLOSUM_[(unsigned char)s1[j]]];
+                j += segLen;
+            }
+        }
+    }
+
+#if DEBUG
+    printf("array length (s1Len(=%d)+0)*s2Len(=%d) = %d\n",
+            s1Len, s2Len, (s1Len+0)*s2Len);
+    int *array = new int[segLen*8*s2Len];
+    init_vect(segLen*8, array, -9);
+#endif
+
+    /* the max alignment score */
+    DP_t max;
+
+    /* array to record the largest score of each reference position */
+    uint16_t* maxColumn = (uint16_t*) calloc(s2Len, 2);
+
+    /* Define 16 byte 0 vector. */
+    __m128i vZero = _mm_setzero_si128();
+    __m128i vOne = _mm_set1_epi16(1);
+
+    __m128i* pvHStore = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvHLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvE = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvHmax = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvMStore = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvMLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvLStore = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvLLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
+
+    /* 16 byte insertion begin vector */
+    __m128i vGapO = _mm_set1_epi16(open);
+
+    /* 16 byte insertion extension vector */
+    __m128i vGapE = _mm_set1_epi16(gap);
+
+    /* Trace the highest score of the whole SW matrix. */
+    __m128i vMaxScore = vZero;
+
+    /* Trace the highest score till the previous column. */
+    __m128i vMaxMark = vZero;
+
+    __m128i vTemp;
+
+    /* outer loop over database sequence */
+    for (int32_t j=0; j<s2Len; ++j) {
+        int32_t cmp;
+        __m128i e;
+        /* Initialize F value to 0.  Any errors to vH values will be corrected
+         * in the Lazy_F loop.  */
+        __m128i vF = vZero;
+        __m128i Nmatch = vZero;
+        __m128i Nlength = vZero;
+        __m128i Cmatch = vZero;
+        __m128i Clength = vZero;
+
+        /* load final segment of pvHStore and shift left by 2 bytes */
+        __m128i vH = _mm_slli_si128(pvHStore[segLen - 1], 2);
+
+        /* load left and upper left match counts */
+        __m128i Wmatch = _mm_load_si128(pvMStore);
+        __m128i NWmatch = _mm_slli_si128(pvMStore[segLen - 1], 2);
+
+        /* load left and upper left length counts */
+        __m128i Wlength = _mm_load_si128(pvLStore);
+        __m128i NWlength = _mm_slli_si128(pvLStore[segLen - 1], 2);
+
+        /* vMaxColumn is used to record the max values of column j. */
+        __m128i vMaxColumn = vZero;
+
+        /* Correct part of the vProfile */
+        const __m128i* vP = vProfile + MAP_BLOSUM_[(unsigned char)s2[j]] * segLen;
+
+        /* Swap the 2 H buffers. */
+        __m128i* pv = pvHLoad;
+        pvHLoad = pvHStore;
+        pvHStore = pv;
+
+        /* Swap the 2 M buffers. */
+        pv = pvMLoad;
+        pvMLoad = pvMStore;
+        pvMStore = pv;
+
+        /* Swap the 2 L buffers. */
+        pv = pvLLoad;
+        pvLLoad = pvLStore;
+        pvLStore = pv;
+
+        /* inner loop to process the query sequence */
+        for (int32_t i=0; i<segLen; ++i) {
+            vH = _mm_adds_epi16(vH, _mm_load_si128(vP + i));
+
+            /* Get max from vH, vE and vF. */
+            e = _mm_load_si128(pvE + i);
+            vH = _mm_max_epi16(vH, e);
+            vH = _mm_max_epi16(vH, vF);
+            vMaxColumn = _mm_max_epi16(vMaxColumn, vH);
+
+            /* Save vH values. */
+            _mm_store_si128(pvHStore + i, vH);
+
+            /* Save vM and vL values */
+            __m128i case1not = _mm_or_si128(
+                    _mm_cmplt_epi16(vH,vF),_mm_cmplt_epi16(vH,e));
+            __m128i case2not = _mm_cmplt_epi16(vF,e);
+            __m128i case2 = _mm_andnot_si128(case2not,case1not);
+            __m128i case3 = _mm_and_si128(case1not,case2not);
+            Cmatch = _mm_andnot_si128(case1not,
+                    _mm_add_epi16(NWmatch, _mm_and_si128(
+                            // fix me _mm_cmpeq_epi16(vs1,vs2),vOne)));
+                            _mm_cmpeq_epi16(vOne,vOne),vOne)));
+            Clength= _mm_andnot_si128(case1not,
+                    _mm_add_epi16(NWlength, vOne));
+            Cmatch = _mm_or_si128(Cmatch, _mm_and_si128(case2, Nmatch));
+            Clength= _mm_or_si128(Clength,_mm_and_si128(case2,
+                        _mm_add_epi16(Nlength, vOne)));
+            Cmatch = _mm_or_si128(Cmatch, _mm_and_si128(case3, Wmatch));
+            Clength= _mm_or_si128(Clength,_mm_and_si128(case3,
+                        _mm_add_epi16(Wlength, vOne)));
+            __m128i mask = _mm_cmplt_epi16(vH,vOne);
+            vH = _mm_andnot_si128(mask, vH);
+            Cmatch = _mm_andnot_si128(mask, Cmatch);
+            Clength= _mm_andnot_si128(mask, Clength);
+            mask = _mm_cmpgt_epi16(vH,vScore);
+            vScore = _mm_andnot_si128(mask, vScore);
+            vScore = _mm_or_si128(vScore, _mm_and_si128(mask, vH));
+            vMatch = _mm_andnot_si128(mask, vMatch);
+            vMatch = _mm_or_si128(vMatch, _mm_and_si128(mask, Cmatch));
+            vLength= _mm_andnot_si128(mask, vLength);
+            vLength= _mm_or_si128(vLength,_mm_and_si128(mask, Clength));
+
+#if DEBUG
+            arr_store_si128(array, vH, i, segLen, j, s2Len);
+#endif
+
+            /* Update vE value. */
+            /* saturation arithmetic, result >= 0 */
+            vH = _mm_subs_epu16(vH, vGapO);
+            e = _mm_subs_epu16(e, vGapE);
+            e = _mm_max_epi16(e, vH);
+            _mm_store_si128(pvE + i, e);
+
+            /* Update vF value. */
+            vF = _mm_subs_epu16(vF, vGapE);
+            vF = _mm_max_epi16(vF, vH);
+
+            /* Load the next vH. */
+            vH = _mm_load_si128(pvHLoad + i);
+        }
+
+        /* Lazy_F loop: has been revised to disallow adjecent insertion and
+         * then deletion, so don't update E(i, i), learn from SWPS3 */
+        for (int32_t k=0; k<8; ++k) {
+            vF = _mm_slli_si128 (vF, 2);
+            for (int32_t i=0; i<segLen; ++i) {
+                vH = _mm_load_si128(pvHStore + i);
+                vH = _mm_max_epi16(vH, vF);
+                _mm_store_si128(pvHStore + i, vH);
+#if DEBUG
+                arr_store_si128(array, vH, i, segLen, j, s2Len);
+#endif
+                vH = _mm_subs_epu16(vH, vGapO);
+                vF = _mm_subs_epu16(vF, vGapE);
+                if (! _mm_movemask_epi8(_mm_cmpgt_epi16(vF, vH))) goto end;
+            }
+        }
+
+end:
+        vMaxScore = _mm_max_epi16(vMaxScore, vMaxColumn);
+        vTemp = _mm_cmpeq_epi16(vMaxMark, vMaxScore);
+        cmp = _mm_movemask_epi8(vTemp);
+        if (cmp != 0xffff) {
+            uint16_t temp;
+            vMaxMark = vMaxScore;
+            max8(temp, vMaxScore);
+            vMaxScore = vMaxMark;
+
+            if (temp > max.score) {
+                max.score = temp;
+                for (int32_t i=0; i<segLen; ++i) {
+                    pvHmax[i] = pvHStore[i];
+                }
+            }
+        }
+
+        /* Record the max score of current column. */
+        max8(maxColumn[j], vMaxColumn);
+    }
+
+    free(vProfile);
+    free(pvHmax);
+    free(pvE);
+    free(pvHLoad);
+    free(pvHStore);
+
+    free(maxColumn);
+
+#if DEBUG
+    print_array(array, s2, s2Len, s1, s1Len, 0);
+    delete [] array;
+#endif
+
+    return max;
+}
+
+
+static int sg(
         const char * const restrict s1, const int s1Len,
         const char * const restrict s2, const int s2Len,
         const int open, const int gap,
@@ -588,7 +814,7 @@ end:
 }
 
 
-int nw(
+static int nw(
         const char * const restrict s1, const int s1Len,
         const char * const restrict s2, const int s2Len,
         const int open, const int gap,
@@ -804,6 +1030,7 @@ int main(int /*argc*/, char ** /*argv*/)
 
     ::std::cout << "alg\t\ttime\tscore\tmatches\tlength" << ::std::endl;
 
+#if 0
     timer = timer_start();
     for (i=0; i<limit; ++i) {
         score = nw(seqA, lena, seqB, lenb, 10, 1, blosum62__);
@@ -817,6 +1044,7 @@ int main(int /*argc*/, char ** /*argv*/)
     }
     timer = timer_end(timer);
     ::std::cout << "sg orig\t\t" << timer/limit << "\t" << score << ::std::endl;
+#endif
 
     timer = timer_start();
     for (i=0; i<limit; ++i) {
@@ -824,6 +1052,18 @@ int main(int /*argc*/, char ** /*argv*/)
     }
     timer = timer_end(timer);
     ::std::cout << "sw orig\t\t" << timer/limit << "\t" << score << ::std::endl;
+
+    timer = timer_start();
+    for (i=0; i<limit; ++i) {
+        stats = sw_stats(seqA, lena, seqB, lenb, 10, 1, blosum62__);
+    }
+    timer = timer_end(timer);
+    ::std::cout << "sw stat\t"
+        << "\t" << timer/limit
+        << "\t" << stats.score
+        << "\t" << stats.matches
+        << "\t" << stats.length
+        << ::std::endl;
 
     return 0;
 }
