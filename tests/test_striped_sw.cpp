a4d848e381ffeb3577a095dfe3b6d211e5026c82
Jeff Daily <jeff.daily@pnnl.gov>
2014-09-05 12:13:17 -0700

    score-only striped sg and nw

--- tests/test_striped_sw.cpp
+++ tests/test_striped_sw.cpp
@@ -16,7 +16,7 @@
 
 using namespace ::std;
 
-#define DEBUG 1
+#define DEBUG 0
 #define DEBUG_MATCHES 0
 #define DEBUG_LENGTH 0
 #define SHORT_TEST 0
@@ -51,6 +51,17 @@ static inline void init_vect(int len, int *vec, int val)
 
 
 #if DEBUG
+static inline int extract(const __m128i &m, const int &pos)
+{
+    union {
+        __m128i m;
+        int16_t v[8];
+    } tmp;
+    tmp.m = m;
+    return tmp.v[pos];
+}
+
+
 static void arr_store_si128(
         int *array,
         __m128i vH,
@@ -59,14 +70,14 @@ static void arr_store_si128(
         int32_t d,
         int32_t dlen)
 {
-    array[(0*seglen+t)*dlen + d] = ((int16_t*)&vH)[0];
-    array[(1*seglen+t)*dlen + d] = ((int16_t*)&vH)[1];
-    array[(2*seglen+t)*dlen + d] = ((int16_t*)&vH)[2];
-    array[(3*seglen+t)*dlen + d] = ((int16_t*)&vH)[3];
-    array[(4*seglen+t)*dlen + d] = ((int16_t*)&vH)[4];
-    array[(5*seglen+t)*dlen + d] = ((int16_t*)&vH)[5];
-    array[(6*seglen+t)*dlen + d] = ((int16_t*)&vH)[6];
-    array[(7*seglen+t)*dlen + d] = ((int16_t*)&vH)[7];
+    array[(0*seglen+t)*dlen + d] = extract(vH, 0);
+    array[(1*seglen+t)*dlen + d] = extract(vH, 1);
+    array[(2*seglen+t)*dlen + d] = extract(vH, 2);
+    array[(3*seglen+t)*dlen + d] = extract(vH, 3);
+    array[(4*seglen+t)*dlen + d] = extract(vH, 4);
+    array[(5*seglen+t)*dlen + d] = extract(vH, 5);
+    array[(6*seglen+t)*dlen + d] = extract(vH, 6);
+    array[(7*seglen+t)*dlen + d] = extract(vH, 7);
 }
 
 
@@ -307,7 +318,7 @@ int sw(
         /* vMaxColumn is used to record the max values of column j. */
         __m128i vMaxColumn = vZero;
 
-        /* Right part of the vProfile */
+        /* Correct part of the vProfile */
         const __m128i* vP = vProfile + MAP_BLOSUM_[s2[j]] * segLen;
 
         /* Swap the 2 H buffers. */
@@ -433,24 +444,18 @@ int sg(
     init_vect(segLen*8, array, -9);
 #endif
 
+    int32_t offset = (s1Len - 1) % segLen;
+    int32_t position = 7 - (s1Len - 1) / segLen;
+
     /* the max alignment score */
-    uint16_t max = NEG_INF;
-
-    /* array to record the largest score of each reference position */
-    uint16_t* maxColumn = (uint16_t*) calloc(s2Len, 2);
-
-    /* Define 16 byte 0 vector. */
-    __m128i vZero = _mm_setzero_si128();
+    int16_t maxLastRow = NEG_INF;
 
     /* Define 16 byte NEG_INF vector. */
-    __m128i vShortMin = _mm_set1_epi16(NEG_INF);
-    print_m128i_16("vShortMin", vShortMin);
-    printf("\n");
+    __m128i vNegInf = _mm_set1_epi16(NEG_INF);
 
     __m128i* pvHStore = (__m128i*) calloc(segLen, sizeof(__m128i));
     __m128i* pvHLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
     __m128i* pvE = (__m128i*) calloc(segLen, sizeof(__m128i));
-    __m128i* pvHmax = (__m128i*) calloc(segLen, sizeof(__m128i));
 
     /* initialize E */
     t = (int16_t*)pvE;
@@ -466,29 +471,18 @@ int sg(
     /* 16 byte insertion extension vector */
     __m128i vGapE = _mm_set1_epi16(gap);
 
-    /* Trace the highest score of the whole SW matrix. */
-    __m128i vMaxScore = vShortMin;
-
-    /* Trace the highest score till the previous column. */
-    __m128i vMaxMark = vShortMin;
-
-    __m128i vTemp;
-
     /* outer loop over database sequence */
     for (int32_t j=0; j<s2Len; ++j) {
         int32_t cmp;
         __m128i e;
         /* Initialize F value to -INF.  Any errors to vH values will be
          * corrected in the Lazy_F loop.  */
-        __m128i vF = vShortMin;
+        __m128i vF = vNegInf;
 
         /* load final segment of pvHStore and shift left by 2 bytes */
         __m128i vH = _mm_slli_si128(pvHStore[segLen - 1], 2);
 
-        /* vMaxColumn is used to record the max values of column j. */
-        __m128i vMaxColumn = vShortMin;
-
-        /* Right part of the vProfile */
+        /* Correct part of the vProfile */
         const __m128i* vP = vProfile + MAP_BLOSUM_[s2[j]] * segLen;
 
         /* Swap the 2 H buffers. */
@@ -504,13 +498,9 @@ int sg(
             e = _mm_load_si128(pvE + i);
             vH = _mm_max_epi16(vH, e);
             vH = _mm_max_epi16(vH, vF);
-            vMaxColumn = _mm_max_epi16(vMaxColumn, vH);
 
             /* Save vH values. */
             _mm_store_si128(pvHStore + i, vH);
-#if DEBUG
-            arr_store_si128(array, vH, i, segLen, j, s2Len);
-#endif
 
             /* Update vE value. */
             vH = _mm_subs_epi16(vH, vGapO);
@@ -536,51 +526,241 @@ int sg(
                 vH = _mm_load_si128(pvHStore + i);
                 vH = _mm_max_epi16(vH, vF);
                 _mm_store_si128(pvHStore + i, vH);
-#if DEBUG
-                arr_store_si128(array, vH, i, segLen, j, s2Len);
-#endif
                 vH = _mm_subs_epi16(vH, vGapO);
                 vF = _mm_subs_epi16(vF, vGapE);
                 if (! _mm_movemask_epi8(_mm_cmpgt_epi16(vF, vH))) goto end;
             }
         }
-
 end:
-        vMaxScore = _mm_max_epi16(vMaxScore, vMaxColumn);
-        vTemp = _mm_cmpeq_epi16(vMaxMark, vMaxScore);
-        cmp = _mm_movemask_epi8(vTemp);
-        if (cmp != 0xffff) {
-            uint16_t temp;
-            vMaxMark = vMaxScore;
-            max8(temp, vMaxScore);
-            vMaxScore = vMaxMark;
-
-            if (temp > max) {
-                max = temp;
-                for (int32_t i=0; i<segLen; ++i) {
-                    pvHmax[i] = pvHStore[i];
-                }
-            }
+        /* extract last value from the column */
+        vH = _mm_load_si128(pvHStore + offset);
+        for (int32_t k=0; k<position; ++k) {
+            vH = _mm_slli_si128 (vH, 2);
         }
-
-        /* Record the max score of current column. */
-        max8(maxColumn[j], vMaxColumn);
+        /* max of last value in each column */
+        int16_t tmp = (signed short) _mm_extract_epi16 (vH, 7);
+        maxLastRow = MAX(maxLastRow, tmp);
+#if DEBUG
+        for (int32_t i=0; i<segLen; ++i) {
+            vH = _mm_load_si128(pvHStore + i);
+            arr_store_si128(array, vH, i, segLen, j, s2Len);
+        }
+#endif
     }
 
+    /* max of last column */
+    __m128i vMaxLastCol = vNegInf;
+    __m128i vQIndex = _mm_set_epi16(
+            7*segLen,
+            6*segLen,
+            5*segLen,
+            4*segLen,
+            3*segLen,
+            2*segLen,
+            1*segLen,
+            0*segLen);
+    __m128i vQLimit = _mm_set1_epi16(s1Len);
+    __m128i vOne = _mm_set1_epi16(1);
+
+    for (int32_t i=0; i<segLen; ++i) {
+        /* load the last stored values */
+        __m128i vH = _mm_load_si128(pvHStore + i);
+        /* mask off the values that were padded */
+        __m128i vMask = _mm_cmplt_epi16(vQIndex, vQLimit);
+        vH = _mm_or_si128(_mm_and_si128(vMask, vH),
+                          _mm_andnot_si128(vMask, vNegInf));
+        vMaxLastCol = _mm_max_epi16(vMaxLastCol, vH);
+        vQIndex = _mm_adds_epi16(vQIndex, vOne);
+    }
+    int16_t maxLastCol;
+    max8(maxLastCol, vMaxLastCol);
+
     free(vProfile);
-    free(pvHmax);
     free(pvE);
     free(pvHLoad);
     free(pvHStore);
-
-    free(maxColumn);
 
 #if DEBUG
     print_array(array, s2, s2Len, s1, s1Len, 0);
     delete [] array;
 #endif
 
-    return max;
+    return MAX(maxLastCol,maxLastRow);
+}
+
+
+int nw(
+        const char * const restrict s1, const int s1Len,
+        const char * const restrict s2, const int s2Len,
+        const int open, const int gap,
+        const int8_t * const restrict matrix)
+{
+    /* compute 'query' profile; s1 is the query */
+    const int32_t n = 24; /* number of amino acids in table */
+    int32_t segLen = (s1Len + 7) / 8;
+    __m128i* vProfile = (__m128i*)malloc(n * segLen * sizeof(__m128i));
+    int16_t* t = (int16_t*)vProfile;
+
+    /* Generate query profile rearrange query sequence & calculate the weight
+     * of match/mismatch */
+    for (int32_t nt=0; nt<n; ++nt) {
+        for (int32_t i=0; i<segLen; ++i) {
+            int32_t j = i;
+            for (int32_t segNum=0; segNum<8; ++segNum) {
+                *t++ = j>=s1Len ? 0 : matrix[nt*n + MAP_BLOSUM_[s1[j]]];
+                j += segLen;
+            }
+        }
+    }
+
+#if DEBUG
+    printf("array length (s1Len(=%d)+0)*s2Len(=%d) = %d\n",
+            s1Len, s2Len, (s1Len+0)*s2Len);
+    int *array = new int[segLen*8*s2Len];
+    init_vect(segLen*8, array, -9);
+#endif
+
+    int32_t offset = (s1Len - 1) % segLen;
+    int32_t position = 7 - (s1Len - 1) / segLen;
+
+    /* the max alignment score */
+    int16_t last_value = NEG_INF;
+
+    /* Define 16 byte NEG_INF vector. */
+    __m128i vNegInf = _mm_set1_epi16(NEG_INF);
+
+    __m128i* pvHStore = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvHLoad = (__m128i*) calloc(segLen, sizeof(__m128i));
+    __m128i* pvE = (__m128i*) calloc(segLen, sizeof(__m128i));
+    int16_t* boundary = (int16_t*) calloc(s2Len, sizeof(int16_t));
+
+    /* initialize H */
+    {
+        t = (int16_t*)pvHStore;
+        for (int32_t i=0; i<segLen; ++i) {
+            int32_t j = i;
+            for (int32_t segNum=0; segNum<8; ++segNum) {
+                *t++ = j>=s1Len ? NEG_INF : -open-gap*(segNum*segLen+i);
+                j += segLen;
+            }
+        }
+    }
+
+    /* initialize uppder boundary */
+    {
+        boundary[0] = 0;
+        for (int32_t i=1; i<s2Len; ++i) {
+            boundary[i] = -open-gap*(i-1);
+        }
+    }
+
+    /* initialize E */
+    t = (int16_t*)pvE;
+    for (int32_t i=0; i<segLen; ++i) {
+        for (int32_t segNum=0; segNum<8; ++segNum) {
+            *t++ = NEG_INF;
+        }
+    }
+
+    /* 16 byte insertion begin vector */
+    __m128i vGapO = _mm_set1_epi16(open);
+
+    /* 16 byte insertion extension vector */
+    __m128i vGapE = _mm_set1_epi16(gap);
+
+    /* outer loop over database sequence */
+    for (int32_t j=0; j<s2Len; ++j) {
+        int32_t cmp;
+        __m128i e;
+        /* Initialize F value to -INF.  Any errors to vH values will be
+         * corrected in the Lazy_F loop.  */
+        __m128i vF = vNegInf;
+
+        /* load final segment of pvHStore and shift left by 2 bytes */
+        __m128i vH = _mm_slli_si128(pvHStore[segLen - 1], 2);
+
+        /* insert upper boundary condition */
+        vH = _mm_insert_epi16(vH, boundary[j], 0);
+
+        /* Correct part of the vProfile */
+        const __m128i* vP = vProfile + MAP_BLOSUM_[s2[j]] * segLen;
+
+        /* Swap the 2 H buffers. */
+        __m128i* pv = pvHLoad;
+        pvHLoad = pvHStore;
+        pvHStore = pv;
+
+        /* inner loop to process the query sequence */
+        for (int32_t i=0; i<segLen; ++i) {
+            vH = _mm_adds_epi16(vH, _mm_load_si128(vP + i));
+
+            /* Get max from vH, vE and vF. */
+            e = _mm_load_si128(pvE + i);
+            vH = _mm_max_epi16(vH, e);
+            vH = _mm_max_epi16(vH, vF);
+
+            /* Save vH values. */
+            _mm_store_si128(pvHStore + i, vH);
+
+            /* Update vE value. */
+            vH = _mm_subs_epi16(vH, vGapO);
+            e = _mm_subs_epi16(e, vGapE);
+            e = _mm_max_epi16(e, vH);
+            _mm_store_si128(pvE + i, e);
+
+            /* Update vF value. */
+            vF = _mm_subs_epi16(vF, vGapE);
+            vF = _mm_max_epi16(vF, vH);
+
+            /* Load the next vH. */
+            vH = _mm_load_si128(pvHLoad + i);
+        }
+
+        /* Lazy_F loop: has been revised to disallow adjecent insertion and
+         * then deletion, so don't update E(i, i), learn from SWPS3 */
+        for (int32_t k=0; k<8; ++k) {
+            vF = _mm_slli_si128 (vF, 2);
+            /* first vF value must be -INF */
+            vF = _mm_insert_epi16(vF, NEG_INF, 0);
+            for (int32_t i=0; i<segLen; ++i) {
+                vH = _mm_load_si128(pvHStore + i);
+                vH = _mm_max_epi16(vH, vF);
+                _mm_store_si128(pvHStore + i, vH);
+                vH = _mm_subs_epi16(vH, vGapO);
+                vF = _mm_subs_epi16(vF, vGapE);
+                if (! _mm_movemask_epi8(_mm_cmpgt_epi16(vF, vH))) goto end;
+            }
+        }
+end:
+        {
+        }
+#if DEBUG
+        for (int32_t i=0; i<segLen; ++i) {
+            vH = _mm_load_si128(pvHStore + i);
+            arr_store_si128(array, vH, i, segLen, j, s2Len);
+        }
+#endif
+    }
+
+    /* extract last value from the last column */
+    __m128i vH = _mm_load_si128(pvHStore + offset);
+    for (int32_t k=0; k<position; ++k) {
+        vH = _mm_slli_si128 (vH, 2);
+    }
+    last_value = (signed short) _mm_extract_epi16 (vH, 7);
+
+    free(vProfile);
+    free(pvE);
+    free(pvHLoad);
+    free(pvHStore);
+    free(boundary);
+
+#if DEBUG
+    print_array(array, s2, s2Len, s1, s1Len, 0);
+    delete [] array;
+#endif
+
+    return last_value;
 }
 
 
@@ -627,14 +807,12 @@ int main(int argc, char **argv)
 
     ::std::cout << "alg\t\ttime\tscore\tmatches\tlength" << ::std::endl;
 
-#if 0
     timer = timer_start();
     for (i=0; i<limit; ++i) {
-        score = sw(seqA, lena, seqB, lenb, 10, 1, blosum62__);
+        score = nw(seqA, lena, seqB, lenb, 10, 1, blosum62__);
     }
     timer = timer_end(timer);
-    ::std::cout << "sw orig\t\t" << timer/limit << "\t" << score << ::std::endl;
-#endif
+    ::std::cout << "nw orig\t\t" << timer/limit << "\t" << score << ::std::endl;
 
     timer = timer_start();
     for (i=0; i<limit; ++i) {
@@ -642,6 +820,13 @@ int main(int argc, char **argv)
     }
     timer = timer_end(timer);
     ::std::cout << "sg orig\t\t" << timer/limit << "\t" << score << ::std::endl;
+
+    timer = timer_start();
+    for (i=0; i<limit; ++i) {
+        score = sw(seqA, lena, seqB, lenb, 10, 1, blosum62__);
+    }
+    timer = timer_end(timer);
+    ::std::cout << "sw orig\t\t" << timer/limit << "\t" << score << ::std::endl;
 
     return 0;
 }
