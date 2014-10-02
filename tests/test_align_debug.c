937061062c9e7b5c4f1d399ead66887f317803c1
Jeff Daily <jeff.daily@pnnl.gov>
2014-10-02 15:29:35 -0700

    add nw_stats_wozniak_128_8.c

--- tests/test_align_debug.c
+++ tests/test_align_debug.c
@@ -98,6 +98,14 @@ int main(int argc, char **argv)
     memset(mch_tbl, 0, sizeof(int)*tbl_size);
     memset(len_tbl, 0, sizeof(int)*tbl_size);
 
+    score = nw_stats_wozniak_128_8_debug(seqA, lena, seqB, lenb, 10, 1, blosum62, &matches, &length, tbl_pr, del_pr, mch_pr, len_pr, scr_tbl, mch_tbl, len_tbl);
+    print_array("nw_stats_wozniak_128_8_scr.txt", scr_tbl, seqA, lena, seqB, lenb);
+    print_array("nw_stats_wozniak_128_8_mch.txt", mch_tbl, seqA, lena, seqB, lenb);
+    print_array("nw_stats_wozniak_128_8_len.txt", len_tbl, seqA, lena, seqB, lenb);
+    memset(scr_tbl, 0, sizeof(int)*tbl_size);
+    memset(mch_tbl, 0, sizeof(int)*tbl_size);
+    memset(len_tbl, 0, sizeof(int)*tbl_size);
+
     score = nw_stats_striped_128_16_debug(seqA, lena, seqB, lenb, 10, 1, blosum62__, &matches, &length, scr_tbl, mch_tbl, len_tbl);
     print_array("nw_stats_striped_128_16_scr.txt", scr_tbl, seqA, lena, seqB, lenb);
     print_array("nw_stats_striped_128_16_mch.txt", mch_tbl, seqA, lena, seqB, lenb);
